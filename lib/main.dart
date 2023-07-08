15f59d9d6d9c88a2d2203b99a280bba9d8512c84
mihcristina <contato@michellicristina.com.br>
2023-07-08 19:35:57 -0300

    Configura http

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=0911f5b8";
+final Uri url = Uri.parse(request);
+
+void main() async {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
+}
